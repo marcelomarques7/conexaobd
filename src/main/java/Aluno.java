96cb01931459cccd054a226906162a66fd384e25
marcelomarques7 <egs.marcelo@gmail.com>
2021-09-29 22:53:32 -0300

    iniciando com JPA

--- src/main/java/Aluno.java
+++ src/main/java/Aluno.java
@@ -1,8 +1,19 @@
+import javax.persistence.*;
+
+@Entity
 public class Aluno {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column
     private String nome;
+
+    @Column
     private int idade;
+
+    @Column
     private String estado;
 
     public Aluno(int id, String nome, int idade, String estado) {
